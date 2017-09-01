066f7ff82ca1833445d769b52f4cc09f04407c11
Adam Selker <aselker@openmailbox.org>
2017-09-01 10:59:16 -0400

    Added some preliminary code

--- bike-light/bike-light.ino
+++ bike-light/bike-light.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+static int ledPins[] = {3, 5, 6, 9, 10, 11};
+static int ledCount = 6;
+static int buttonPin = 2;
+static int bounceDelay = 10000; //10 milliseconds
 
+int mode = 0;
+//Modes: Off, On, Dim, Flash, Scan
+
+void setup() {
+  for (int i = 0; i < ledCount; i++) pinMode(ledPins[i], OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(buttonPin), switchMode, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  for (int i = 0; i < ledCount; i++) {
+    switch (mode) {
+      case 0:
+        digitalWrite(ledPins[i], LOW);
+        break;
+      case 1:
+        digitalWrite(ledPins[i], HIGH);
+        break;
+      case 2:
+        analogWrite(ledPins[i], 127); //Half brightness
+      default:
+        //Do nothing
+        break;
+
+    } //End swtich
+
+}
+
+void switchMode() {
+
+  mode++;
+  if (mode > 2) mode = 0;
+  delayMicroseconds(bounceDelay); //Debouncing
 
 }
