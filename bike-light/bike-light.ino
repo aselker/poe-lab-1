8f026e3353d3b1fef38da2ab6954300bd0455e94
Adam Selker <aselker@openmailbox.org>
2017-09-05 11:28:53 -0400

    Added potentiometer.

--- bike-light/bike-light.ino
+++ bike-light/bike-light.ino
@@ -1,25 +1,37 @@
 static int ledPins[] = {3, 5, 6, 9, 10, 11};
 static int ledCount = 6;
 static int buttonPin = 2;
+static int potPin = A0;
 static long int bounceDelay = 50000; //microseconds
+static float speedMult = 5;
 
 int mode = 0;
 //Modes: Off, On, Dim, Flash, Scan
 
+unsigned long int lastTime;
+int timer = 0;
+
 void switchMode() {
 
+  noInterrupts();
   mode++;
-  if (mode > 5) mode = 0;
+  if (mode > 6) mode = 0;
   delayMicroseconds(bounceDelay); //Debouncing
   while (digitalRead(buttonPin) == HIGH) delayMicroseconds(10000); //Wait until the button is released
   delayMicroseconds(bounceDelay); //Debouncing
+  interrupts();
 
 }
 
 void setup() {
   for (int i = 0; i < ledCount; i++) pinMode(ledPins[i], OUTPUT);
+  pinMode(potPin, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(buttonPin), switchMode, RISING);
+
+  lastTime = millis();
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -36,22 +48,30 @@ void loop() {
         analogWrite(ledPins[i], 127); //Half brightness
         break;
       case 3:
-        if ((millis() % 1000) > 500) digitalWrite(ledPins[i], HIGH);
+        if ((timer % 1000) > 500) digitalWrite(ledPins[i], HIGH);
         else digitalWrite(ledPins[i], LOW);
         break;
       case 4:
-        analogWrite(ledPins[i], ((sin(float(millis()) / 1000.0 * 2.0*PI)) + 1) / 2 * 256);
+        analogWrite(ledPins[i], ((sin(float(timer) / 1000.0 * 2.0*PI)) + 1) / 2 * 256);
         break;
       case 5:
-        if (((millis() + (1000 / ledCount)*i) % 1000) > ( 1000 - 1000 / ledCount)) digitalWrite(ledPins[i], HIGH);
+        if (((timer + (1000 / ledCount)*i) % 1000) > ( 1000 - 1000 / ledCount)) digitalWrite(ledPins[i], HIGH);
         else digitalWrite(ledPins[i], LOW);
         break;
+      case 6:
+        analogWrite(ledPins[i], ((sin(float(timer + (1000 / ledCount*i)) / 1000.0 * 2.0*PI)) + 1) / 2 * 256);
       default:
         //Do nothing
         break;
 
     } //End swtich
   } //End for
+
+  timer += int( (millis() - lastTime) * (float(analogRead(potPin)) * speedMult / 1024.0) );
+  timer = timer % 1000;
+  lastTime = millis();
+
+  Serial.println(lastTime);
 
 }
 
